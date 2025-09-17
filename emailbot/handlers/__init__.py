2d2c4e83bb1cc24d3225803499a0539eb8af5636
Vlada <91487261+taravskayavm@users.noreply.github.com>
2025-09-17 09:01:29 +0300

    Extract manual send handlers into dedicated package

---
+++ emailbot/handlers/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from .manual_send import (
+    start,
+    manual_mode,
+    select_group,
+    proceed_to_group,
+    send_all,
+)
+
+__all__ = [
+    "start",
+    "manual_mode",
+    "select_group",
+    "proceed_to_group",
+    "send_all",
+]
