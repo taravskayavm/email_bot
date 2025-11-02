9eb46cbc73ff0fd2449e4d582689eddc0aa991bb
Vlada <91487261+taravskayavm@users.noreply.github.com>
2025-11-02 19:14:46 +0300

    Handle spawn initialization issues

--- emailbot/__init__.py
+++ emailbot/__init__.py
@@ -1,4 +1,11 @@
-"""Helpers for the email bot."""
+import multiprocessing
+# предотвращает рекурсивный импорт при spawn
+if multiprocessing.current_process().name != "MainProcess":
+    import sys
+
+    sys.modules["emailbot"] = None
+
+__doc__ = """Helpers for the email bot."""
 
 import importlib
 
